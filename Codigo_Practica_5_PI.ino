70d26412858bd2fb8efd7fcb86e0d0227e1abb45
Diego <80914655+d1ego38@users.noreply.github.com>
2024-05-10 16:32:51 -0600

    Add files via upload

---
+++ Codigo_Practica_5_PI.ino
@@ -0,0 +1,76 @@
+const int A = 2;
+const int B = 3;
+const int C = 4;
+const int D = 5;
+const int E = 6;
+const int F = 7;
+const int G = 8;
+
+const int N = 7;
+const int SEGMENTOS[N] = {A,B,C,D,E,F,G};
+
+//MATRIZ CATODO COMUN
+const int DIGITOS[10][N] = {
+  {0,1,1,0,0,0,0},
+  {1,1,1,1,0,0,1},
+  {1,0,1,1,0,1,1},
+  {1,1,1,0,0,0,0},
+  {1,1,1,0,0,1,1}
+};
+const int ON = HIGH;
+
+float Kg = 2;
+float Lb;
+float Amperios = 2.98;
+float microAmperios;
+
+float multiplicacionKilogramos(float Kg) {
+  return Kg * 2.20462;
+}
+
+float multiplicacion_uA(float microAmperios){
+  return Amperios * 1000000;
+}
+
+
+
+void setup()
+{
+  Serial.begin(9600); 
+  Lb = multiplicacionKilogramos(Kg);
+  Serial.print(Kg);
+  Serial.print(" kilogramos es igual a: ");
+  Serial.print(Lb);
+  Serial.println(" Libras ");
+  delay(1000);
+  
+  microAmperios = multiplicacion_uA(Amperios);
+  Serial.print(Amperios);
+  Serial.print(" Amperios son iguales a: ");
+  Serial.print(microAmperios);
+  Serial.println(" uA ");
+  delay(1000);
+
+
+  for(int i = 0; i <= 9; i++) {
+    pinMode(SEGMENTOS[i], OUTPUT);
+    digitalWrite(SEGMENTOS[i], LOW);
+  }
+}
+
+void print(int d){
+  for(int i = 0; i < N; i++){
+    digitalWrite(SEGMENTOS[i], DIGITOS[d][i]);
+  }
+}
+
+void loop()
+{
+
+  for(int i = 0; i <= 9; i++) {
+    print(i);
+    delay(1000);
+
+    }
+    
+}
---
+++ Codigo_Practica_5_PIII.ino
@@ -0,0 +1,65 @@
+#define push_button 10
+#define buzzer 9
+#define RELE 11 
+
+const int A = 2;
+const int B = 3;
+const int C = 4;
+const int D = 5;
+const int E = 6;
+const int F = 7;
+const int G = 8;
+
+const int N = 7;
+const int SEGMENTOS[N] = {A,B,C,D,E,F,G};
+
+//MATRIZ CATODO COMUN
+const int DIGITOS[10][N] = {
+  {1,1,1,1,1,1,0},
+  {0,1,1,0,0,0,0},
+  {1,1,0,1,1,0,1},
+  {1,1,1,1,0,0,1},
+  {0,1,1,0,0,1,1},
+  {1,0,1,1,0,1,1},
+  {1,0,1,1,1,1,1},
+  {1,1,1,0,0,0,0},
+  {1,1,1,1,1,1,1},
+  {1,1,1,0,0,1,1}
+};
+const int ON = HIGH;
+
+
+void setup()
+{
+  
+  pinMode(push_button, INPUT); 
+  pinMode(buzzer, OUTPUT);
+  pinMode(RELE, OUTPUT);
+  for(int i = 9; i >= 0; i--) {
+    pinMode(SEGMENTOS[i], OUTPUT);
+    digitalWrite(SEGMENTOS[i], LOW);
+  }
+}
+
+void print(int d){
+  for(int i = 0; i < N; i++){
+    digitalWrite(SEGMENTOS[i], DIGITOS[d][i]);
+  }
+}
+
+void loop()
+{
+  while (digitalRead(push_button) == LOW);
+  for(int i = 9; i >= 0; i--) {
+    print(i);
+    delay(1000);
+    if(i == 0){
+    tone(buzzer, 600 );
+    digitalWrite(RELE, LOW);
+    delay(500);
+    noTone(buzzer);
+    digitalWrite(RELE, HIGH);
+    }
+    
+  }
+}
